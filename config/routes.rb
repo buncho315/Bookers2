953df9f54ad5681d73afb830a4c0321b9ee22a29
buncho315 <mzh.hrtk125@gmail.com>
2022-06-19 08:20:55 +0000

    Initial Commit3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
   root to: "homes#top"
   devise_for :users
+  get "about" => "top#about" , as: "about" #自分で追加
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
