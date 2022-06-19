4318437bdb49995a7ac83817f562f44fe4e34dd7
buncho315 <mzh.hrtk125@gmail.com>
2022-06-19 13:11:15 +0000

    Initial Commit4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "about" => "top#about" , as: "about" #自分で追加
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :images, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
 end
