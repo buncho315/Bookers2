d54f66f4230a3f9b0a210641ed0519480d511a23
buncho315 <mzh.hrtk125@gmail.com>
2022-06-20 12:56:55 +0000

    Initial Commit5

--- app/models/image.rb
+++
@@ -1,4 +0,0 @@
-class Image < ApplicationRecord
-  has_one_attached :image
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   devise_for :users
-  get "about" => "top#about" , as: "about" #自分で追加
+  get "about" => "homes#about" , as: "about"
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'users/:id' => 'users#update', as: 'update_user'
+  #自分で追加
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  resources :users, only: [:show, :edit, :index]
+
 end
